55efda7e9448a9407d29afc20699408e58888e93
Lazar Nikolov <lazar.nikolov@sentry.io>
2022-11-21 17:22:54 +0100

    implementing user feedback

--- app/src/main/java/com/nikolovlazar/sentryuserfeedback/MainActivity.kt
+++ app/src/main/java/com/nikolovlazar/sentryuserfeedback/MainActivity.kt
@@ -3,26 +3,55 @@ package com.nikolovlazar.sentryuserfeedback
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nikolovlazar.sentryuserfeedback.ui.theme.SentryUserFeedbackTheme
+import io.sentry.Sentry
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
+            val snackbarHostState = remember { SnackbarHostState() }
             SentryUserFeedbackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                NavHost(navController = navController, startDestination = "main") {
+                    composable("main") {
+                        Scaffold(
+                            snackbarHost = { SnackbarHost(snackbarHostState) },
+                            content = { innerPadding ->
+                                Surface(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(innerPadding),
+                                    color = MaterialTheme.colors.background
+                                ) {
+                                    TriggerError(snackbarHostState, navController)
+                                }
+                            }
+                        )
+                    }
+                    composable("reportBug/{eventId}") { backStackEntry ->
+                        ReportBug(
+                            snackbarHostState,
+                            navController,
+                            backStackEntry.arguments?.getString("eventId")
+                        )
+                    }
                 }
             }
         }
@@ -30,14 +59,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TriggerError(snackbarHostState: SnackbarHostState, navController: NavController) {
+    val coroutineScope = rememberCoroutineScope()
+    val onClick: () -> Unit = {
+        try {
+            // faulty method
+            throw Exception("CRASH")
+        } catch (e: Exception) {
+            val eventId = Sentry.captureException(e)
+            coroutineScope.launch {
+                val snackbarResult = snackbarHostState.showSnackbar(
+                    message = "Oh no \uD83D\uDE27",
+                    actionLabel = "Report this!"
+                )
+                when (snackbarResult) {
+                    SnackbarResult.Dismissed -> {}
+                    SnackbarResult.ActionPerformed -> {
+                        // Navigate to new screen
+                        navController.navigate("reportBug/$eventId")
+                    }
+                }
+            }
+        }
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Button(onClick = onClick) {
+            Text("Unleash chaos!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SentryUserFeedbackTheme {
-        Greeting("Android")
+        val snackbarHostState = remember { SnackbarHostState() }
+        val navController = rememberNavController()
+        Scaffold(
+            snackbarHost = { SnackbarHost(snackbarHostState) },
+            content = { innerPadding ->
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TriggerError(snackbarHostState, navController)
+                }
+            }
+        )
     }
 }
---
+++ app/src/main/java/com/nikolovlazar/sentryuserfeedback/ReportBug.kt
@@ -0,0 +1,165 @@
+package com.nikolovlazar.sentryuserfeedback
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import io.sentry.Sentry
+import io.sentry.UserFeedback
+import io.sentry.protocol.SentryId
+
+@Preview
+@Composable
+fun ReportBug(
+    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() },
+    navController: NavController = rememberNavController(),
+    eventId: String? = ""
+) {
+    val context = LocalContext.current
+    var titleState by remember { mutableStateOf("") }
+    var whatHappenedState by remember { mutableStateOf("") }
+    var whatShouldveHappenedState by remember { mutableStateOf("") }
+    var nameState by remember { mutableStateOf("") }
+    var emailState by remember { mutableStateOf("") }
+
+    if (eventId == null) {
+        return
+    }
+
+    val sendFeedback: () -> Unit = {
+        val userFeedback = UserFeedback(SentryId(eventId)).apply {
+            name = nameState
+            email = emailState
+            comments = """Title: $titleState
+==============================================
+What Happened: $whatHappenedState
+==============================================
+What Should've Happened: $whatShouldveHappenedState"""
+        }
+        Sentry.captureUserFeedback(userFeedback)
+
+        Toast.makeText(
+            context,
+            "Feedback sent. Thank you \uD83D\uDC96",
+            Toast.LENGTH_LONG
+        ).show()
+        navController.popBackStack()
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(
+                            Icons.Default.ArrowBack,
+                            contentDescription = "Go back"
+                        )
+                    }
+                },
+                title = {
+                    Text(text = "Report a bug")
+                },
+            )
+        },
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(top = 16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                CustomTextField(
+                    value = titleState,
+                    onValueChange = { value -> titleState = value },
+                    placeholder = "Title",
+                )
+                CustomTextField(
+                    value = whatHappenedState,
+                    onValueChange = { value -> whatHappenedState = value },
+                    placeholder = "What happened?",
+                    maxLines = 5,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .height(120.dp)
+                )
+                CustomTextField(
+                    value = whatShouldveHappenedState,
+                    onValueChange = { value -> whatShouldveHappenedState = value },
+                    placeholder = "What should've happened?",
+                    maxLines = 5,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .height(120.dp)
+                )
+                CustomTextField(
+                    value = nameState,
+                    onValueChange = { value -> nameState = value },
+                    placeholder = "Name",
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                )
+                CustomTextField(
+                    value = emailState,
+                    onValueChange = { value -> emailState = value },
+                    placeholder = "Email",
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                    ),
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                )
+                Button(
+                    onClick = sendFeedback,
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Text("Send")
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
+    maxLines: Int = 1,
+) {
+    TextField(
+        value = value,
+        onValueChange = { newValue -> onValueChange(newValue) },
+        placeholder = { Text(placeholder) },
+        maxLines = maxLines,
+        keyboardOptions = keyboardOptions,
+        shape = RoundedCornerShape(16.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color(0, 0, 0, 15),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        modifier = modifier
+    )
+}
